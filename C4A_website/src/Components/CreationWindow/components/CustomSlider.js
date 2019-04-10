d9e1099d01e17eaf2b1eeaf615a04ca92f7ea926
Jean Pougetoux <Jean.Pougetoux@fmc-ag.com>
2019-04-10 11:51:01 +0200

    slider ok

--- C4A_website/src/Components/CreationWindow/components/CustomSlider.js
+++ C4A_website/src/Components/CreationWindow/components/CustomSlider.js
@@ -6,30 +6,12 @@ import styles from '../css/Grid.css'
 import Tooltip from 'rc-tooltip';
 import Slider from 'rc-slider';
 
-const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Handle = Slider.Handle;
-
-const handle = (props) => {
-  const { value, dragging, index, ...restProps } = props;
-  return (
-    <Tooltip
-      prefixCls="rc-slider-tooltip"
-      overlay={value}
-      visible={dragging}
-      placement="top"
-      key={index}
-    >
-      <Handle value={value} {...restProps} />
-    </Tooltip>
-  );
-};
 
 class CustomSlider extends Component {
 
   handle (props){
     const { value, dragging, index, ...restProps } = props;
-
-    this.props.changeSize(value);
 
     return (
       <Tooltip
@@ -44,10 +26,14 @@ class CustomSlider extends Component {
     );
   };
 
+  changeValue(e) {
+    this.props.changeSize(e);
+  }
+
   render() {
     return (
-        <div class="custom-slider">
-            <Slider min={0} max={100} defaultValue={30} handle={this.handle.bind(this)} />
+        <div className="custom-slider">
+            <Slider min={0} max={100} defaultValue={30} handle={this.handle.bind(this)} onChange={this.changeValue.bind(this)} />
         </div>
     );
   }
