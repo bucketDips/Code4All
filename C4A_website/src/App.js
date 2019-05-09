a94c8617bd7e0e9fd38509bc5cae4e433a230274
Jean Pougetoux <Jean.Pougetoux@fmc-ag.com>
2019-05-09 16:38:29 +0200

    adding jwt-decode

--- C4A_website/src/App.js
+++ C4A_website/src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import HomeWindow from './Components/HomeWindow/';
 import CreateExerciseWindow from './Components/CreationWindow/';
 import ErrorWindow from './Components/ErrorWindow/';
+import LoginWindow from './Components/LoginWindow/';
+import decode from 'jwt-decode';
 
+/*const checkAuth = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  if(!token || !refreshToken) {
+    return false;
+  }
 
+  try {
+    const { exp } = decode(refreshToken);
+    if(exp < new Date().getTime()) {
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+}
+
+const AuthRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={propos => (
+    checkAuth() ? (
+      <Component {...propos} />
+    ) : (
+      <Redirect to={{ pathname: '/login' }} />
+    )
+  )} />
+)*/
 
 class App extends Component {
 
---
+++ C4A_website/src/Components/LoginWindow/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class LoginWindow extends Component {
+  render() {
+    return (
+        <div className="login-window">
+            <h3 className="title">log</h3>
+            <div className="content">
+            
+            </div>
+        </div>
+    );
+  }
+}
+
+export default LoginWindow;
