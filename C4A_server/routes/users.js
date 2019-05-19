046ea25996a10f7bb2e907c78ddcaf5aebdea670
Robin SOLDE <robin.solde@gmail.com>
2019-05-19 12:30:18 +0200

    test webhook jenkins2

--- C4A_server/routes/users.js
+++ C4A_server/routes/users.js
@@ -260,7 +260,7 @@ router.get('/connect/:email/:pwd', function(request, res, next) {
 				jsonResponse = {
 					success: true,
 					code : 'AUTHENTIFICATION_SUCCESS',
-					message: 'authentification réussi mamen!',
+					message: 'authentification réussi mamen2!',
 					token: token
 				}
 				
