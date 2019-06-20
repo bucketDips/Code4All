46c9d9fd62d76d33d0ef57d84911072b17551ad5
Robin SOLDE <robin.solde@gmail.com>
2019-06-21 00:52:09 +0200

    exercices routes

--- C4A_server/routes/exercices.js
+++ C4A_server/routes/exercices.js
@@ -27,6 +27,21 @@ var funcString = [
 var createFunction = function (txt) {
     return new Function("return " + txt)();
 }
+
+function insertExerciceFunctions(functions, exercice_id) {
+    return new Promise(function(resolve, reject) {
+        var reccords = [];
+        for (var i = 0; i < functions.length; ++i){
+            reccords.push([exercice_id, escapeQuote(functions[i].code),escapeQuote(functions[i].name) + "()"],escapeQuote(functions[i].description))
+        }
+        console.log(reccords)
+        var sql = "INSERT INTO exercice_functions (exercice_id, code, name, description) VALUES ?"
+        con.query(sql, [reccords],function (err, rows, fields) {
+            if (err) return reject(err);
+            resolve(rows);
+        });
+    });
+}
 /* GET users listing. */
 router.get('/test', function(request, res, next) {
     var id = request.params.id;
@@ -48,8 +63,39 @@ router.get('/test', function(request, res, next) {
 
     res.send("toto");
 });
-router.get('/getExercice/:id', function(request, res, next) {
+router.get('/getUserExercices', AUTH.VERIFYAUTH,function(request, res, next) {
+    var userId = request.decoded.id
+    function getUserPersonnalExercices(userId) {
+        return new Promise(function(resolve, reject) {
+            var sql = "select * from exercices where author_id = ?;"
+            con.query(sql, [userId], function (err, rows, fields) {
+                if (err) return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+    function getUserForkedExercices(userId) {
+        return new Promise(function(resolve, reject) {
+            var sql = "select * from exercices, user_exercices where user_exercices.userid = ? and exercices.id = user_exercices.exerciceId;"
+            con.query(sql, [userId], function (err, rows, fields) {
+                if (err) return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+    getUserPersonnalExercices(userId).then(function(rowsPersonnal){
+        getUserForkedExercices(userId).then(function(rowsForked){
+            res.json({
+                perso:rowsPersonnal,
+                forked:rowsForked
+            })
+
+        })
+    })
+})
+router.get('/getExercice/:id', AUTH.VERIFYAUTH,function(request, res, next) {
     var id = request.params.id;
+    var userId = request.decoded.id
     var temp = {}
     function getExercice(id) {
         return new Promise(function(resolve, reject) {
@@ -71,6 +117,20 @@ router.get('/getExercice/:id', function(request, res, next) {
         });
     }
     getExercice(id).then(function(rows){
+        if (rows.length == 0){
+            return res.status(404).json({
+                success: false,
+                code : 'NOT_FOUND',
+                message: 'Cet exercice n\'existe pas !'
+            })
+        }
+        if (rows[0].author_id != userId && rows[0].public == 0){
+            return res.status(403).json({
+                success: false,
+                code : 'MISSING_AUTHORISATION',
+                message: 'Cet exercice ne vous appartient pas et n\'est pas public!'
+            })
+        }
         getFunctions(id).then(function(rows1){
             var resultJson = {
                 exercice: rows[0],
@@ -83,7 +143,7 @@ router.get('/getExercice/:id', function(request, res, next) {
 var getFuncName = function(str){
     return str.substring(0,str.indexOf('('))
 }
-router.post('/executeExercice', function(request, res, next) {
+router.post('/executeExercice', AUTH.VERIFYAUTH,function(request, res, next) {
     // var exercice = JSON.parse(request.body.exercice);
     var exercice = ""
     var functionList = {};
@@ -199,30 +259,24 @@ router.post('/executeExercice', function(request, res, next) {
     res.send("toto")
 });
 var escapeQuote = function(str){
-    return str.replace("'", "''")
+    var find = "'";
+    var re = new RegExp(find, 'g');
+    return str.replace(re, "''")
 }
-router.post('/add/:title', function(request, res, next) {
-    var title = request.params.title;
-    var text = "toto"
-    var public = 1;
-    var author_id=1;
-    // var contentOjb = {"lines":6,"columns":20,"patternId":8,"blocks":[{"id":0,"row":6,"column":1,"width":10,"height":1,"patternId":8},{"id":1,"row":6,"column":11,"width":10,"height":1,"patternId":8},{"id":2,"row":3,"column":8,"width":1,"height":1,"patternId":4},{"id":3,"row":3,"column":9,"width":1,"height":1,"patternId":3},{"id":4,"row":3,"column":10,"width":1,"height":1,"patternId":4},{"id":5,"row":3,"column":11,"width":1,"height":1,"patternId":3}],"npcs":[{"id":0,"row":5,"column":15,"width":1,"height":1,"patternId":6}],"pcs":[{"id":0,"row":5,"column":5,"width":1,"height":1,"patternId":2,"functions":[{"name":"i","code":"function oki() {\r\n  return \"A\";\r\n}","description":"o"}]}],"labels":[]}
-    // var contentOjb = {"lines":6,"columns":20,"patternId":8,"blocks":[{"id":0,"row":6,"column":1,"width":10,"height":1,"patternId":8},{"id":1,"row":6,"column":11,"width":10,"height":1,"patternId":8},{"id":2,"row":3,"column":8,"width":1,"height":1,"patternId":4},{"id":3,"row":3,"column":9,"width":1,"height":1,"patternId":3},{"id":4,"row":3,"column":10,"width":1,"height":1,"patternId":4},{"id":5,"row":3,"column":11,"width":1,"height":1,"patternId":3}],"npcs":[{"id":0,"row":5,"column":15,"width":1,"height":1,"patternId":6}],"pcs":[{"id":0,"row":5,"column":5,"width":1,"height":1,"patternId":2,"functions":[{"name":"couille","code":"function couille() {\r\n  console.log(\"c'est une belle couille\");\r\n}","description":"ma couille"}]}],"labels":[]}
+router.post('/add', AUTH.VERIFYAUTH,function(request, res, next) {
     var contentOjb =JSON.parse(request.body.exercice);
-    // console.log(request)
     var content = JSON.stringify(contentOjb)
-    console.log("contentOjb")
-    console.log(contentOjb.pcs)
-    console.log("contentOjb")
-    // console.log(content)
-    // console.log("content")
-    function insertExercice(title,text,public,content, author_id) {
+    var author_id = request.decoded.id
+
+
+    function insertExercice(contentOjb,content,author_id) {
         return new Promise(function(resolve, reject) {
-            title = escapeQuote(title)
-            text = escapeQuote(text)
+            contentOjb.title = escapeQuote(contentOjb.title)
+            contentOjb.text = escapeQuote(contentOjb.text)
             content = escapeQuote(content)
-            var sql = "insert into exercices(title,text,isPublic,content, author_id)"
-            sql += "values('"+title+"','"+text+"','"+public+"','"+content+"','"+author_id+"')"+";";
+            console.log(content)
+            var sql = "insert into exercices(title,text,isPublic,content, author_id,code)"
+            sql += "values('"+contentOjb.title+"','"+contentOjb.text+"','"+contentOjb.public+"','"+content+"','"+author_id+"','"+contentOjb.code+"')"+";";
             console.log(sql)
             con.query(sql, function (err, rows, fields) {
                 if (err) return reject(err);
@@ -231,22 +285,9 @@ router.post('/add/:title', function(request, res, next) {
 
         });
     }
-    function insertExerciceFunctions(functions, exercice_id) {
-        return new Promise(function(resolve, reject) {
-            var reccords = [];
-            for (var i = 0; i < functions.length; ++i){
-                reccords.push([exercice_id, functions[i].code,functions[i].name + "()"])
-            }
-            console.log(reccords)
-            var sql = "INSERT INTO exercice_functions (exercice_id, code, name) VALUES ?"
-            con.query(sql, [reccords],function (err, rows, fields) {
-                if (err) return reject(err);
-                resolve(rows);
-            });
-        });
-    }
-    insertExercice(title,text,public,content, author_id).then(function(rows){
-        insertExerciceFunctions(contentOjb.pcs[0].functions,rows.insertId).then(function(rows){
+
+    insertExercice(contentOjb,content,author_id).then(function(rows){
+        insertExerciceFunctions(contentOjb.functions,rows.insertId).then(function(rows){
             res.send(rows);
         });
     });
