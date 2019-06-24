6c305730d38e726820e5b356d1084c632cbb7b4d
Robin SOLDE <robin.solde@gmail.com>
2019-06-24 20:45:05 +0200

    escapequote

--- C4A_server/routes/exercices.js
+++ C4A_server/routes/exercices.js
@@ -395,22 +395,23 @@ var escapeQuote1 = function(str){
 function escapeQuote (str) {
     return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
         switch (char) {
-            // case "\0":
-            //     return "\\0";
-            // case "\x08":
-            //     return "\\b";
-            // case "\x09":
-            //     return "\\t";
-            // case "\x1a":
-            //     return "\\z";
-            // case "\n":
-            //     return "";
-            // case "\r":
-            //     return "";
-            // case "\"":
+            case "\0":
+                return "\\0";
+            case "\x08":
+                return "\\b";
+            case "\x09":
+                return "\\t";
+            case "\x1a":
+                return "\\z";
+            case "\n":
+                return "\\n";
+            case "\r":
+                return "\\r";
             case "'":
-            // case "\\":
-            // case "%":
+                return "''"
+            case "\"":
+            case "\\":
+            case "%":
                 return "\\"+char; // prepends a backslash to backslash, percent,
                                   // and double/single quotes
         }
