806d20733bf797aec40245c64cf5ef6790459c51
Robin SOLDE <robin.solde@gmail.com>
2019-06-24 21:20:12 +0200

    adding string instead of json

--- C4A_server/routes/exercices.js
+++ C4A_server/routes/exercices.js
@@ -438,8 +438,8 @@ router.post('/add', AUTH.VERIFYAUTH,function(request, res, next) {
             console.log(content)
             var sql = "insert into exercices(title,text,isPublic,author_id,code,blocks,columns,labels,lineS,npcs,patternId,pcs) values (?)"
             var values = [];
-            values.push(contentOjb.title,contentOjb.text,contentOjb.public,author_id,escapeQuote(contentOjb.code),contentOjb.blocks, contentOjb.columns)
-            values.push(contentOjb.labels, contentOjb.lines, contentOjb.npcs, contentOjb.patternId, contentOjb.pcs)
+            values.push(contentOjb.title,contentOjb.text,contentOjb.public,author_id,escapeQuote(contentOjb.code),JSON.stringify(contentOjb.blocks), contentOjb.columns)
+            values.push(JSON.stringify(contentOjb.labels), contentOjb.lines, JSON.stringify(contentOjb.npcs), contentOjb.patternId, JSON.stringify(contentOjb.pcs))
             console.log(sql)
             con.query(sql, [values], function (err, rows, fields) {
                 if (err) return reject(err);
