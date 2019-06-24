95c2ea91be5806b4148166a21e3e09e5f4b027d2
Robin SOLDE <robin.solde@gmail.com>
2019-06-24 16:32:55 +0200

    extend bodu max size

--- C4A_server/app.js
+++ C4A_server/app.js
@@ -28,8 +28,8 @@ app.options('*', cors())
 //   next();
 // });
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
