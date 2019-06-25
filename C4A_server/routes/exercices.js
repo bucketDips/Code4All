2fc9d8a7f150763e15b8c3fcb58741aac78a8e21
Robin SOLDE <robin.solde@gmail.com>
2019-06-25 14:29:36 +0200

    another escape quote

--- C4A_server/routes/exercices.js
+++ C4A_server/routes/exercices.js
@@ -276,7 +276,7 @@ router.get('/getExercice/:id', AUTH.VERIFYAUTH,function(request, res, next) {
                 rows[0].code =  rows[0].code.replace(re, ";\n")
                 find = "rn"
                 re = new RegExp(find, 'g');
-                rows[0].code =  rows[0].code.replace(re, ";\n")
+                rows[0].code =  rows[0].code.replace(re, "\n")
                 rows[0].blocks = JSON.parse(stand(rows[0].blocks))
                 rows[0].npcs = JSON.parse(stand(rows[0].npcs))
                 rows[0].pcs = JSON.parse(stand(rows[0].pcs))
