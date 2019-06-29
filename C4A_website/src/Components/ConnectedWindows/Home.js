b58e8afd5d3bda4868faf198721eebdfa2f8fa4e
JeanPougetoux <spyroo913@hotmail.com>
2019-06-29 23:23:16 +0200

    remove alert

--- C4A_website/src/Components/ConnectedWindows/Home.js
+++ C4A_website/src/Components/ConnectedWindows/Home.js
@@ -5,7 +5,6 @@ import ConnectedWindowsStructure from '../ConnectedWindowsStructure/';
 class HomeWindow extends Component {
 
   render() {
-    alert("robin est un con");
     return (
         <ConnectedWindowsStructure type="home" />
     );
