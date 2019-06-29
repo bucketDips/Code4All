57d9f4fd3a1ade916b9c90c4362883b699e23f9c
JeanPougetoux <spyroo913@hotmail.com>
2019-06-29 21:24:55 +0200

    change port

--- C4A_website/src/Components/GestionStoreWindow/index.js
+++ C4A_website/src/Components/GestionStoreWindow/index.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import Exercice from './Exercice';
 import style from './style.css';
 
 class GestionStoreWindow extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-        
-    }
+  componentWillMount() {
+    this.setState({exercices: this.props.exercices});
   }
 
   render() {
+    var exercices = this.state.exercices.map(exercice => {
+      return (<Exercice 
+        id={exercice.exerciceId} 
+        title={exercice.title} 
+        author={exercice.authorName} 
+        authorId={exercice.author_id} 
+        description={exercice.description} />)
+    });
     return (
         <div className={style.store} style={{backgroundImage: "url(" + process.env.PUBLIC_URL + "blackboard.jpg)", backgroundSize: "cover"}}>
-            coucou
+            {exercices}
         </div>
     );
   }
